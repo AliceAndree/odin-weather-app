62661686e88f321054711b168aea25c6fe9c6773
AliceAndree <thiel.alice@gmail.com>
2025-05-02 15:15:20 +0200

    fetching all data when searching for a location

--- src/index.js
+++ src/index.js
@@ -1,4 +1,53 @@
 import './styles.css';
 import fetchWeatherData from './modules/weatherAPI';
 
-fetchWeatherData();
+const submitLocation = document.querySelector('#submit-location');
+
+submitLocation.addEventListener('click', () => {
+  const location = document.querySelector('#search-location').value;
+  getWeatherData(location);
+});
+
+const getWeatherData = async (location) => {
+  const data = await fetchWeatherData(location);
+  console.log(data);
+  getCurrentTemperature(data);
+  getCurrentCondition(data);
+  getFeelsLike(data);
+  getConditionByHour(data);
+  getNextDaysPrevisions(data);
+};
+
+const getCurrentTemperature = (weatherData) => {
+  const currentTemp = weatherData.currentConditions.temp;
+  console.log(`Current Temp: ${currentTemp}`);
+};
+
+const getCurrentCondition = (weatherData) => {
+  const currentCondition = weatherData.currentConditions.conditions;
+  console.log(`Current condition: ${currentCondition}`);
+};
+
+const getFeelsLike = (weatherData) => {
+  const feelsLike = weatherData.currentConditions.feelslike;
+  console.log(`Feels like: ${feelsLike}`);
+};
+
+const getConditionByHour = (weatherData) => {
+  const conditionByHour = weatherData.days[0].hours;
+  const hours = [2, 5, 8, 11, 14, 17, 20, 23];
+
+  for (let i = 0; i < hours.length; i++) {
+    const hoursData = conditionByHour[hours[i]];
+    console.log(hoursData);
+  }
+};
+
+const getNextDaysPrevisions = (weatherData) => {
+  const nextDaysPrevisions = weatherData.days;
+
+  for (let i = 0; i < 8; i++) {
+    const dayData = nextDaysPrevisions[i];
+    console.log(dayData);
+  }
+};
--- src/modules/weatherAPI.js
+++ src/modules/weatherAPI.js
@@ -1,14 +1,15 @@
-const fetchWeatherData = async () => {
-  const key = 'GD7P5QXY996SPP994RLADPFMW';
+const fetchWeatherData = async (location) => {
+  const key = 'GD7P5QXY996SPP994RLADPFMW'; // Todo : hide key in server
   try {
     const response = await fetch(
-      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/brussels?unitGroup=metric&include=days%2Chours%2Ccurrent&key=${key}&contentType=json`,
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&include=days%2Chours%2Ccurrent&key=${key}&contentType=json`,
       { mode: 'cors' },
     );
     const weatherData = await response.json();
-    console.log(weatherData);
+    return weatherData;
   } catch (error) {
     console.log(error);
+    return null;
   }
 };
 
